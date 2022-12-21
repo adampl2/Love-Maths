6e4814e4e00ea0fb4963136012e3969ea115d006
adampl2 <adamchwiulkowski@gmail.com>
2022-12-21 11:37:24 +0000

    code added to increment correct and incorrect answers.

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Congratulations! You got it right!');
+        incrementScore();
     } else {
         alert(`Wrong answer :/ you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}.`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -72,12 +74,22 @@ function calculateCorrectAnswer() {
     }
 }
 
-function incrementScore() {
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
-function incrementWrongAnswer() {
+/**
+ * Gets the current tally of incorrect answer from the DOM and increments it by 1
+ */
 
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
